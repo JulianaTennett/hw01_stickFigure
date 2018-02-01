4f737c7387db5b6666f093185df9c60805e15ad5
Julie Tennett <tennett.j@gmail.com>
2018-02-01 15:57:34 -0500

    add color scheme and key pressed functionality

--- hw04_drawingApp/sketch.js
+++ hw04_drawingApp/sketch.js
@@ -1,14 +1,30 @@
-//VARIABLES
+//COLORS
+var purple;
+var purpleLight;
+var blue;
+var blueLight;
 var gray;
 var white;
+//VARIABLES
+var strokeColor;
 var toolsX;
 var penY;
 var markerY;
 var eraserY;
+var clearY;
+var panelW;
+var purpleX;
+var blueX;
+var colorD;
+var colorY;
 //BOOLEANS
 var penStroke;
 var markerStroke;
 var eraserStroke;
+var backgroundColor;
+var purpleOn;
+var blueOn;
+var blackOn;
 //IMAGES
 var pen;
 var marker;
@@ -25,64 +41,98 @@ function setup() {
 	createCanvas(windowWidth, windowHeight);
 	gray="#F8F6F6";
 	white="#fffff";
-	toolsX = (windowWidth/6)/2;
-	penY = windowHeight/2;
-	markerY = windowHeight/3.5;
-	eraserY = windowHeight - (windowHeight/8);
+	purple="#F1DCE9";
+	purpleLight="#FBF0F7";
+	blue="#C4E2ED";
+	blueLight="E3F3F9";
+	toolsX = (width/6)/2;
+	penY = height/6;
+	markerY = height/3;
+	eraserY = height - (height/5);
+	clearY = height - (height/9);
+	panelW = width/6;
 	penStroke = false;
 	markerStroke = false;
 	eraserStroke = false;
+	backgroundColor = white;
+	purpleOn = false;
+	blueOn = false;
+	blackOn = true;
+	colorD = 35;
+	colorX = panelW/4;
+	colorY = height/1.75;
 
 	//CANVAS ELEMENTS
-	background(white);
-	noStroke();
-	fill(gray);
-	rect(0, 0, windowWidth/6, windowHeight);
+	background(backgroundColor);
 	textStyle('asap');
-
 }
 
 function draw() {
 	gray="#F8F6F6";
 	white="#fffff";
 
-	stroke(0);
-	strokeWeight(4);
-	strokeCap(ROUND);
-	smooth();
-
 	//TOOL BAR
+	noStroke();
+	fill(gray);
+	rect(0, 0, panelW, height);
 	//tools
 	imageMode(CENTER);
 	image(pen, toolsX, penY, 100, 100);
 	image(marker, toolsX, markerY, 100, 100);
-	//eraser
+	//buttons
 	noStroke();
 	fill(white);
 	rectMode(CENTER);
-	rect(toolsX, eraserY, 100, 50, 5);
+	rect(toolsX, eraserY, 100, 50, 5); //eraser
+	rect(toolsX, clearY, 100, 50, 5); //clear
 	fill(0);
 	textSize(16);
 	textAlign(CENTER);
 	text('ERASER', toolsX, eraserY);
+	text('CLEAR', toolsX, clearY);
+	//colors
+	fill(purple);
+	ellipse(colorX, colorY, colorD, colorD);
+	fill(blue);
+	ellipse(colorX*2, colorY, colorD, colorD);
+	fill(0);
+	ellipse(colorX*3, colorY, colorD, colorD);
+
 
 
 	if (mouseIsPressed) {
-		if (penStroke) {
-			stroke(0, 255);
-			strokeWeight(1);
-		} else if (markerStroke) {
-			stroke(0, 220);
-			strokeWeight(15);
-		} else if (eraserStroke) {
-			stroke(white);
-			strokeWeight(50);
-		} else {
-			noStroke();
+		if (mouseX > windowWidth/6) {
+			if (penStroke) {
+				stroke(0, 255);
+				strokeWeight(3);
+			} else if (markerStroke) {
+				stroke(0, 220);
+				strokeWeight(15);
+			} else if (eraserStroke) {
+				stroke(backgroundColor);
+				strokeWeight(50);
+			} else {
+				noStroke();
+			}
+			if (!eraserStroke) {
+				if (purpleOn) {
+					stroke(purple);
+				} else if (blueOn) {
+					stroke(blue);
+				} else if (blackOn) {
+					stroke(0);
+				}
+			}
+			line(pmouseX, pmouseY, mouseX, mouseY); //draw!
 		}
-		line(pmouseX, pmouseY, mouseX, mouseY); //draw!
 	}
 }
+
+//create button on the colors
+//create color booleans
+//when mouse click on button boolean becomes true or false
+//true or false determines color fill?
+
 
 function mousePressed() {
 	if ((mouseX > toolsX - 50) && (mouseX < toolsX + 50) && (mouseY > penY - 50) && (mouseY < penY + 50)) {
@@ -97,6 +147,34 @@ function mousePressed() {
 		markerStroke = false;
 		penStroke = false;
 		eraserStroke = true;
+		purpleOn = false;
+		blueOn = false;
+		blackOn = false;
+	} else if ((mouseX > toolsX - 50) && (mouseX < toolsX + 50) && (mouseY > clearY - 25) && (mouseY < clearY + 25)) {
+		background(backgroundColor);
+	}
+	if ((mouseX > colorX - colorD/2) && (mouseX < colorX + colorD/2) && (mouseY > colorY - colorD/2) && (mouseY < colorY + colorD/2)) { //turn purple on
+		purpleOn = true;
+		blueOn = false;
+		blackOn = false;
+		backgroundColor = purpleLight;
+		background(backgroundColor);
+	} else if ((mouseX > colorX*2 - colorD/2) && (mouseX < colorX*2 + colorD/2) && (mouseY > colorY - colorD/2) && (mouseY < colorY + colorD/2)) { //turn blue on
+		purpleOn = false;
+		blueOn = true;
+		blackOn = false;
+		backgroundColor = '#E3F3F9';
+		background(backgroundColor);
+	} else if ((mouseX > colorX*3 - colorD/2) && (mouseX < colorX*3 + colorD/2) && (mouseY > colorY - colorD/2) && (mouseY < colorY + colorD/2)) { //turn black on
+		purpleOn = false;
+		blueOn = false;
+		blackOn = true;
+	}
+}
+
+function keyTyped() {
+	if (key === 'c' || key === 'C') {
+		background(backgroundColor);
 	}
 }
 
