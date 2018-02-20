907b5d26294ec510a238bc3e053898209b2203bd
Julie Tennett <tennett.j@gmail.com>
2018-02-20 17:37:38 -0500

    add all data and gradient background function

--- prototype01/data.js
+++ prototype01/data.js
@@ -6,8 +6,8 @@ var data = {
 	pieces: [
 		{ 	id: 1,
 			maniac: false,
-			date: "10/11/2016",
-			message: "u ever get hit with such a huge wave of vague, overwhelming emotions and don’t know how to process the sudden rush of feeling in your stomach so you just wanna scream for no reason but ur in public"
+			date: "10/11/16",
+			message: '"u ever get hit with such a huge wave of vague, overwhelming emotions and don’t know how to process the sudden rush of feeling in your stomach so you just wanna scream for no reason but ur in public"'
 		},
 		{
 			id: 2,
@@ -17,22 +17,51 @@ var data = {
 		},
 		{
 			id: 3,
-			maniac: false,
-			date: "",
-			message: '"In cyclothymic disorder, moods swing between short periods of mild depression and hypomania, an elevated mood... People with cyclothymic disorder have milder symptoms than occur in full-blown bipolar disorder."'  // note the use of single quotes here to wrap double quotes!
+			maniac: true,
+			date: "7/3/15",
+			message: '"HYPED UP ON BITMOJIS AND COLLEGE APARTMENT HEAVEN ON CRAIGSLIST.CANNOT SLEEP. CANNOT CALM DOWN. TOO MANY POSSIBILITIES. FUTURE!!!"'  // note the use of single quotes here to wrap double quotes!
 		},
 		{
 			id: 4,
 			maniac : true,
 			date: "",
-			message: 'Cyclothymic disorder, I only just learned that word now. I originally titled this piece "Some Things on Manic Depression"'
+			message: 'My therapist once told me "If people average 50 thoughts per minute, you must average 150" That’s when I realized my mind is a constant rapid whirlwind of thoughts, more thoughts than others usually have, and that’s why I am always buzzing. Always thinking. Always over-thinking.'
 		},
 		{
 			id: 5,
+			maniac: true,
+			date: "12/16/16",
+			message: '"When you’re hyped up on coffee and Scout events and free pizza and your body doesn’t feel like it can contain the emotion...and suddenly your hype is deflated but that energy is still buzzing in your chest and now it’s really negative, embarrassed energy and not excited energy but close to excited, but not excited, it’s sad and now the negative energy is taking up all this space and you want to explode into star dust and tears at the same time"'
+		},
+		{
+			id: 6,
+			maniac: true,
+			date: "4/15/15",
+			message: '"I FEEL REALLY GOOD ABOUT MY LIFE I DONT KNOW WHEN I TRANSITIONED INTO HAPPY AGAIN BUT IM LOVING IT LETS GO ITS A NEW PHASE OF LIFE AND IM FINALLY STARTING TO LET GO OF THE PAST"'
+		},
+		{
+			id: 7,
+			maniac: true,
+			date: "9/23/16",
+			message: '"I AM IN A GREAT MOOD!!!!!!!!!!!!!!!!!!!!!!!!!!!!! IT’S A GREAT DAY!!!!!!!!!!!!!!!!"'
+		},
+		{
+			id: 8,
 			maniac: false,
 			date: "3/30/2016",
-			message: "I don’t understand why my emotions are so, so up and down. In Lisbon I felt like nothing would ever hurt again, and now I’m in Madrid and wondering why I am in Europe at all. I don’t get it. Why do I have to be so manic depressive?"
+			message: '"I don’t understand why my emotions are so, so up and down. In Lisbon I felt like nothing would ever hurt again, and now I’m in Madrid and wondering why I am in Europe at all. I don’t get it. Why do I have to be so manic depressive?"'
+		},
+		{
+			id: 6,
+			maniac: false,
+			date: "8/1/16",
+			message: '"Still fighting off this invading feeling of melancholy and grayness and trying to feel happy and do fun things, and I just want to want to leave my apartment to have fun instead of feeling exhausted and bored and uninterested 100% of the time and why don’t I find joy in things I usually find joy in??!"'
+		},
+		{
+			id: 9,
+			maniac: false,
+			date: "9/18/15",
+			message: '"I feel an inexplicable weight on my face. Like my eyes feel heavier and my cheeks are heavier so I can’t smile or make expressions or do anything but sit dead face. My faces feels dead, that’s it. Which is not cool, because I am supposed to be a happy, bubbly, fun-loving person, who’s always positive and doesn’t let things get to her and keeps her emotions in tact. But here I am, feeling dead"'
 		}
 	]
 };
-
--- prototype01/sketch.js
+++ prototype01/sketch.js
@@ -1,21 +1,31 @@
 var pink = '#F3C7C7';
+var lightBlue = '#B9D8FC';
 var redColor = '#F15E6E';
 var blueColor = '#7E91AB';
 var white = '#fffff';
 var screenMessage = "";
 
+var b1, b2, c1, c2;
+
+var Y_AXIS = 1;
+var X_AXIS = 2;
+var b1, b2, c1, c2;
+
 // Let's replace everything with one array for all balls!
-var allBalls = []; 
+var allBalls = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 	noStroke();
 
+  c1 = color('#F3C7C7');
+  c2 = color('#B9D8FC');
+
   /*
-   Since we want nice spaced even distribution in the X axis for each group of balls, 
+   Since we want nice spaced even distribution in the X axis for each group of balls,
    there is some precalcs we need to do:
    - Figure out how many maniacs/depressed balls are in the `data` object
-   - Generate random numbers for each spacing between balls in the group 
+   - Generate random numbers for each spacing between balls in the group
     (+1 for the last gap to the gap between the last ball and the right of the screen)
    - Find the random total per group and normalize these random numbers for screen values
    - Initiate the balls with those random x values :)
@@ -57,7 +67,7 @@ function setup() {
     depressGaps[i] = width * depressGaps[i] / depressGapsTotal;
   }
 
-  // Let's create as many balls as obejcts in `data`, 
+  // Let's create as many balls as obejcts in `data`,
   // and store the datum object with all the piece information in the Ball directly! :)
   // Lat's also use the randomly generated x coordinates
   let manIt = 0,
@@ -78,7 +88,11 @@ function setup() {
 }
 
 function draw() {
-  background(pink);
+  // background(pink);
+  // var inter = map(i, y, y+h, 0, 1);
+  // var backColor = lerpColor(pink, lightBlue, inter);
+  // background(backColor);
+  setGradient(0, 0, width, height, c1, c2, Y_AXIS);
 
   // Manage all the balls
   for (let i = 0; i < allBalls.length; i++) {
@@ -95,5 +109,27 @@ function draw() {
   rectMode(CENTER);
   text(screenMessage, width/2, height/2, 500, 100);
 	pop();
+
 }
 
+function setGradient(x, y, w, h, c1, c2, axis) {
+
+  noFill();
+
+  if (axis == Y_AXIS) {  // Top to bottom gradient
+    for (var i = y; i <= y+h; i++) {
+      var inter = map(i, y, y+h, 0, 1);
+      var c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(x, i, x+w, i);
+    }
+  }
+  else if (axis == X_AXIS) {  // Left to right gradient
+    for (var i = x; i <= x+w; i++) {
+      var inter = map(i, x, x+w, 0, 1);
+      var c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(i, y, i, y+h);
+    }
+  }
+}
